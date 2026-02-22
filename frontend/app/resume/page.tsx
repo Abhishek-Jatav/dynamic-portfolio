5c890b4e3e58afc12ecb9d78a39f418e0a43d6cb
Abhishek Jatav <abhidel44@gmail.com>
2026-02-23 00:29:56 +0530

    polish-ui

--- frontend/app/resume/page.tsx
+++ frontend/app/resume/page.tsx
@@ -1,5 +1,10 @@
 import PremiumResume from "@/components/resume/PremiumResume";
 
 export default function Resume() {
-  return <PremiumResume />;
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4">Under Construction</h1>
+        {/* <PremiumResume /> */}
+    </>
+  );
 }
