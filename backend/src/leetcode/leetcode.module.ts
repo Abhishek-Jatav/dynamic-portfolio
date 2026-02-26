6fe86a086b038450a9dee1915bf6af6029f20d18
Abhishek Jatav <abhidel44@gmail.com>
2026-02-26 14:48:38 +0530

    update data fetching in leetcode and github

--- backend/src/leetcode/leetcode.module.ts
+++ backend/src/leetcode/leetcode.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LeetcodeService } from './leetcode.service';
 import { LeetcodeController } from './leetcode.controller';
+import { Leetcode, LeetcodeSchema } from './leetcode.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Leetcode.name, schema: LeetcodeSchema },
+    ]),
+  ],
   controllers: [LeetcodeController],
   providers: [LeetcodeService],
 })
